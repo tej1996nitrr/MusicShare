d05618a8efc797954d21f84bab27703dd8d4dae7
tej1996nitrr <tejaskanikdaley1996@gmail.com>
2020-09-13 12:30:04 +0530

    fmt

--- music/tracks/schema.py
+++ music/tracks/schema.py
@@ -14,14 +14,18 @@ class Query(graphene.ObjectType):
 class CreateTrack(graphene.Mutation):
     track = graphene.Field(TrackType)
     class Arguments:
+        # Defines the data you can send to the server, in this case, the title, description, url
         title = graphene.String()
         description = graphene.String()
         url = graphene.String()
+
+    # mutation method: it creates a link in the database using the data sent by the user,
     def mutate(self,info,title,description,url):
         track = Tracks(title=title,description=description,url=url)
         track.save()
         return CreateTrack(track=track)
-
+        
+# Creates a mutation class with a field to be resolved, which points to our mutation defined befor
 class Mutation(graphene.ObjectType):
     create_track = CreateTrack.Field()
         
